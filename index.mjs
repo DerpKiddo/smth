9250dcf8ff412710e6378c614fedf7ab940e6bb1
DerpKiddo <75645436+DerpKiddo@users.noreply.github.com>
2023-06-24 23:22:42 -0400

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -12,7 +12,11 @@ const agent = new HttpsProxyAgent('http://xtristannK8NJ:WzuHjcjinTNh3cC4@proxy.s
 
 server.on('request', async (req, res) => {
   // Pass the agent to the fetch function to route the request through the proxy
-  const asset = await fetch(url + req.url, { agent });
+  const headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
+  };
+
+  const asset = await fetch(url + req.url, { agent, headers });
   const body = Buffer.from(await asset.arrayBuffer());
   res.writeHead(asset.status, { "Content-Type": asset.headers.get("content-type").split(";")[0] });
   res.end(body);
